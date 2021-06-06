1640c146c558f1c923241094d36fc99cacddf41b
ramonahauenschild <80481452+ramonahauenschild@users.noreply.github.com>
2021-06-06 21:04:50 +0200

    noch mehr skripts

---
+++ CoronaJump&Run/Assets/Skripten/Pause.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+   [SerializeField] GameObject pauseMenu;
+
+   public void pausieren()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void Menu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Hauptmenü");
+    }
+}
---
+++ CoronaJump&Run/Assets/Skripten/destroyplayer2.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class destroyplayer2 : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Debug.Log("Trigger!");
+            SceneManager.LoadScene("GameOver2");
+        }
+        if (other.CompareTag("Finish"))
+        {
+            SceneManager.LoadScene("LevelFinish");
+        }
+    }
+}
